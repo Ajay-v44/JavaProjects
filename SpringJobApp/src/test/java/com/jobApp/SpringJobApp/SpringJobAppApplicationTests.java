65bed95abf0abeec2450c0eb72ce2551e7ab00e0
Ajay v <115262085+Ajay-v44@users.noreply.github.com>
2025-10-27 00:42:41 +0530

    initial commit

---
+++ SpringJobApp/src/test/java/com/jobApp/SpringJobApp/SpringJobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobApp.SpringJobApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJobAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
