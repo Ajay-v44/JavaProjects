4870ee11be68f63d900b532bfbbaf536e396a006
Ajay v <vajay88v@gmail.com>
2025-11-11 21:46:38 +0530

    Add image generation endpoint to AiController
    Introduces a new POST /generate endpoint that uses OpenAiImageModel to generate images based on a prompt. The controller now supports both chat and image generation functionalities.

--- AI/src/main/java/com/spring/AI/Controllers/AiController.java
+++ AI/src/main/java/com/spring/AI/Controllers/AiController.java
@@ -1,11 +1,14 @@
 package com.spring.AI.Controllers;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.image.ImageResponse;
 import org.springframework.ai.openai.OpenAiChatModel;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ai.openai.OpenAiImageModel;
+import org.springframework.ai.openai.OpenAiImageOptions;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 import java.util.Objects;
@@ -16,8 +19,11 @@ public class AiController {
 
     private final ChatClient chatClient;
 
-    public AiController(OpenAiChatModel chatModel) {
+    private final OpenAiImageModel openAiImageModel;
+
+    public AiController(OpenAiChatModel chatModel, OpenAiImageModel openAiImageModel) {
         chatClient = ChatClient.create(chatModel);
+        this.openAiImageModel = openAiImageModel;
     }
 //    public AiController(ChatClient.Builder chatClientBuilder) {
 //        this.chatClient = chatClientBuilder.build();
@@ -31,6 +37,22 @@ public class AiController {
         } catch (Exception e) {
             System.out.println(e.getMessage());
             return Map.of("Exception", "Please Try Again");
+        }
+    }
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateImage(@RequestParam String prompt) {
+        try {
+            ImagePrompt imagePrompt = new ImagePrompt(prompt, OpenAiImageOptions.builder()
+                    .quality("hd")
+                    .height(1024)
+                    .width(1024)
+                    .style("natural")
+                    .build());
+            ImageResponse imageResponse = openAiImageModel.call(imagePrompt);
+            return ResponseEntity.status(HttpStatus.OK).body(imageResponse.getResult().getOutput().getUrl());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }
