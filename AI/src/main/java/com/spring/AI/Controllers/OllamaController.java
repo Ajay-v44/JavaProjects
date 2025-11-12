4fffca86d22d8ac7fe695472296793e8147947b8
Ajay v <vajay88v@gmail.com>
2025-11-12 23:05:09 +0530

    Add audio and movie endpoints, enhance OllamaController
    Introduces AudioGenController for speech-to-text and text-to-speech endpoints. Adds Movie DTO and new endpoints in OllamaController for movie recommendations, actor movies, and structured movie data. Also comments out DataInitializer's @PostConstruct to disable automatic data initialization.

--- AI/src/main/java/com/spring/AI/Controllers/OllamaController.java
+++ AI/src/main/java/com/spring/AI/Controllers/OllamaController.java
@@ -1,9 +1,12 @@
 package com.spring.AI.Controllers;
 
+import com.spring.AI.DTO.Movie;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.converter.BeanOutputConverter;
+import org.springframework.ai.converter.ListOutputConverter;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.ollama.OllamaChatModel;
@@ -13,9 +16,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.convert.support.DefaultConversionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -28,9 +34,9 @@ public class OllamaController {
     @Autowired
     public VectorStore vectorStore;
 
-    public OllamaController(@Qualifier("ollamaEmbeddingModel") EmbeddingModel embeddingModel , OllamaChatModel chatModel) {
+    public OllamaController(@Qualifier("ollamaEmbeddingModel") EmbeddingModel embeddingModel, OllamaChatModel chatModel) {
         this.chatClient = ChatClient.create(chatModel);
-        this.embeddingModel=embeddingModel;
+        this.embeddingModel = embeddingModel;
     }
 
     @GetMapping("/ask")
@@ -46,9 +52,9 @@ public class OllamaController {
     }
 
     @PostMapping("/reccomend_movie")
-    public ResponseEntity<String> reccomend(@RequestParam String language,@RequestParam String genre ,@RequestParam int year){
+    public ResponseEntity<String> reccomend(@RequestParam String language, @RequestParam String genre, @RequestParam int year) {
         try {
-            String prompt= """
+            String prompt = """
                     Recommend a list of movies based on the following criteria:
                     - Language: {language}
                     - Genre: {genre}
@@ -62,35 +68,71 @@ public class OllamaController {
                     - Movie Length (in minutes)
                     Please return the results in a structured format (e.g., JSON or table) for easy parsing."
                     """;
-            PromptTemplate promptTemplate=new PromptTemplate(prompt);
-            Prompt pmt=promptTemplate.create(Map.of("language",language,"genre",genre,"year",year));
+            PromptTemplate promptTemplate = new PromptTemplate(prompt);
+            Prompt pmt = promptTemplate.create(Map.of("language", language, "genre", genre, "year", year));
             return ResponseEntity.status(HttpStatus.OK).body(chatClient.prompt(pmt).call().content());
         } catch (Exception e) {
             System.out.println(e.getMessage());
-            return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Exception\", \"Please Try Again");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Exception\", \"Please Try Again");
         }
     }
 
     @PostMapping("/embedd")
-    public ResponseEntity<float[]> embeddText(@RequestParam String text){
-        try{
+    public ResponseEntity<float[]> embeddText(@RequestParam String text) {
+        try {
             return ResponseEntity.status(HttpStatus.OK).body(embeddingModel.embed(text));
         } catch (Exception e) {
             System.out.println(e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
+
     @PostMapping("/product")
     public List<Document> getProducts(@RequestParam String text) {
         //  return vectorStore.similaritySearch(text);
         return vectorStore.similaritySearch(SearchRequest.builder().query(text).topK(2).build());
     }
+
     @PostMapping("/ask/rag")
-    public String askQuestion(@RequestParam String query){
+    public String askQuestion(@RequestParam String query) {
         return chatClient
                 .prompt(query)
                 .advisors(new QuestionAnswerAdvisor(vectorStore))
                 .call()
                 .content();
+    }
+
+    @GetMapping("/getActorMovies/{actorName}")
+    public List<String> getParticularActorMovies(@PathVariable String actorName) {
+        List<String> movies = chatClient.prompt()
+                .user(u -> u.text("List top 5 movies of {name}").param("name", actorName))
+                .call()
+                .entity(new ListOutputConverter(new DefaultConversionService()));
+
+        return movies;
+    }
+
+    @GetMapping("/getMovie/beanOC")
+    public Movie beanOC(@RequestParam String actorName){
+        BeanOutputConverter<Movie> opCon=new BeanOutputConverter<Movie>(Movie.class);
+
+        Movie movie = chatClient.prompt()
+                .user(u -> u.text("Get me the best movie of {name}").param("name", actorName))
+                .call()
+                .entity(new BeanOutputConverter<Movie>(Movie.class));
+
+        return movie;
+    }
+    @GetMapping("/moviesList")
+    public List<Movie> getMovieList(@RequestParam String name){
+
+        BeanOutputConverter<Movie> opCon=new BeanOutputConverter<Movie>(Movie.class);
+
+        List<Movie> movies = chatClient.prompt()
+                .user(u -> u.text("Top 5 movies of {name}").param("name", name))
+                .call()
+                .entity(new BeanOutputConverter<List<Movie>>(new ParameterizedTypeReference<List<Movie>>() {}));
+
+        return movies;
     }
 }
---
+++ AI/src/main/java/com/spring/AI/DTO/Movie.java
@@ -0,0 +1,4 @@
+package com.spring.AI.DTO;
+
+public record Movie(String actorName,Integer year,String actress,String director,String movieName) {
+}
